26a5761c7330c872cd618982ae8e862cdf98d5fa
Sonu Kumar <sonu.kumar@fluper.in>
2022-03-17 17:22:23 +0530

    Add wallet transactions

--- routes/payment.js
+++ routes/payment.js
@@ -2,39 +2,53 @@ var express = require('express');
 const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "ggcpcj7nyhhwpkqg",
-  publicKey: "vsvf6cmzvsmmghjc",
-  privateKey: "dded365087fd8f2dfc3f01a4c37acdc9"
+    environment: braintree.Environment.Sandbox,
+    merchantId: "ggcpcj7nyhhwpkqg",
+    publicKey: "vsvf6cmzvsmmghjc",
+    privateKey: "dded365087fd8f2dfc3f01a4c37acdc9"
 });
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/client_token', function(req, res, next) {
+router.get('/client_token', function (req, res, next) {
     gateway.clientToken.generate({}, (err, response) => {
         res.send("{\"client_token\":\"" + response.clientToken + "\"}");
-      });
+    });
 });
 
 
 /* GET users listing. */
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', function (req, res, next) {
     const nonceFromTheClient = req.body.payment_method_nonce;
+    const merchantAccountId = req.body.merchantAccountId;
+    const requireThreeDSecure = req.body.requireThreeDSecure;
+
+    let threeDSecureObj = {};
+
+    if(requireThreeDSecure == "true"){
+        threeDSecureObj = {
+            required: requireThreeDSecure
+        };   
+    } else {
+        threeDSecureObj = undefined
+    }
 
     gateway.transaction.sale({
         amount: "10.00",
+        merchantAccountId: merchantAccountId,
         paymentMethodNonce: nonceFromTheClient,
         options: {
-          submitForSettlement: true
+            threeDSecure: threeDSecureObj,
+            submitForSettlement: true
         }
-      }, (err, result) => {
+    }, (err, result) => {
         if (result.success) {
-          res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result));
         } else {
-          res.send("error occured" + err);
+            res.send("error occured" + err);
         }
 
-      });
+    });
 });
 
 module.exports = router;
